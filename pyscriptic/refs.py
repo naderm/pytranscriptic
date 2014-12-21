b7a789584da77c9d5fe0b2c555e35cd19f21c4d7
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 18:11:53 -0500

    Changed "store" to "store_where"

--- pyscriptic/refs.py
+++ pyscriptic/refs.py
@@ -3,16 +3,16 @@ from pyscriptic.containers import CONTAINERS
 from pyscriptic.storage import STORAGES
 
 class Reference:
-    def __init__(self, container_id=None, new=None, store=None, discard=False):
+    def __init__(self, container_id=None, new=None, store_where=None, discard=False):
         assert (container_id is not None) != (new is not None)
-        assert (store is not None) != (discard)
-        assert store in STORAGES.keys() + [None]
+        assert (store_where is not None) != (discard)
+        assert store_where in STORAGES.keys() + [None]
         assert new in CONTAINERS.keys() + [None]
 
         # XXX: Check container id?
         self.container_id = container_id
         self.new = new
-        self.store = store
+        self.store = {"where": store_where}
         self.discard = discard
 
     # XXX: Convert to json somehow...
