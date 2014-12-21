a5f1d9668120f79d0ef328d1e8b601320caa5265
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 17:51:39 -0500

    Added missing self argument

--- pyscriptic/refs.py
+++ pyscriptic/refs.py
@@ -3,11 +3,12 @@ from pyscriptic.containers import CONTAINERS
 from pyscriptic.storage import STORAGES
 
 class Reference:
-    def __init__(container_id=None, new=None, store=None, discard=False):
+    def __init__(self, container_id=None, new=None, store=None, discard=False):
         assert (container_id is not None) != (new is not None)
         assert (store is not None) != (discard)
         assert store in STORAGES.keys() + [None]
         assert new in CONTAINERS.keys() + [None]
+
         # XXX: Check container id?
         self.container_id = container_id
         self.new = new
