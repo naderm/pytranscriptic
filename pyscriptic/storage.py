30abb2d516bba51ede68d2d49ea8dd12c24ba1a4
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 16:40:45 -0500

    Added sources of storage for containers

---
+++ pyscriptic/storage.py
@@ -0,0 +1,25 @@
+
+from collections import namedtuple
+
+StorageProperties = namedtuple(
+    "StorageProperties",
+    ["description"]
+    )
+
+STORAGES = {
+    "ambient": StorageProperties(
+        "may vary between 19.5 degC and 22 degC",
+        ),
+    "warm_37": StorageProperties(
+        "may vary between 36 degC and 38 degC, optionally shaking",
+        ),
+    "cold_4": StorageProperties(
+        "may vary between 3 degC and 5 degC",
+        ),
+    "cold_20": StorageProperties(
+        "may vary between -22 degC and -18 degC",
+        ),
+    "cold_80": StorageProperties(
+        "may vary between -84 degC and -76 degC",
+        ),
+    }
