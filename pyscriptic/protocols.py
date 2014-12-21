98ed1efde2fd70ea0a0957695f0bd26405421d52
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 18:21:59 -0500

    Added synthesize_oligo

--- pyscriptic/protocols.py
+++ pyscriptic/protocols.py
@@ -1,10 +1,30 @@
 
-from pytranscriptic import runs
+from pyscriptic import runs
 
 def submit_protocol(refs, instructions, title="PyTranscript Run"):
     request = {
         "refs": refs,
         "instructions": instructions,
+        }
+    runs.run(
+        request,
+        title=title,
+        )
+
+def synthesize_oligo(name, sequence, purity, scale):
+    assert purity in ["desalt", "hplc", "page"]
+    assert scale in ["25:nanomole", "50:nanomole", "200:nanomole",
+                     "1:micromole", "10:micromole"]
+
+    title = "Synthesize {}".format(name)
+    request = {
+        "type": "synthesize",
+        "sequence": {
+            "name": name,
+            "sequence": sequence,
+        },
+        "purity": purity,
+        "scale": scale,
         }
     runs.run(
         request,
