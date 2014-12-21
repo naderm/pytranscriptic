098663d0a8fd5f1eabeea6b1a4a015dabdadb5fb
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 16:21:20 -0500

    Began stubbing instructions

---
+++ pyscriptic/instructions.py
@@ -0,0 +1,24 @@
+
+# Reference: https://www.transcriptic.com/platform/#instructions
+
+# Liquid Handling
+
+# Covers and Sealing
+
+# DNA Sequencing
+
+# Centrifugation
+
+# Thermocycling
+
+# Incubation
+
+# Spectrophotometry
+
+# Gel Electrophoresis
+
+# Flow Cytometry (Coming Soon)
+
+# Liquid Chromatography (Coming soon)
+
+# Mass Spectrometry (Coming soon)
