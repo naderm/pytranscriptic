d540678df8d88a72015fffcdba72d539d186571c
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 23:14:44 -0500

    Added docstrings to submit.py and settings.py Moved get_headers into submit.py and made it private

--- pyscriptic/settings.py
+++ pyscriptic/settings.py
@@ -8,28 +8,55 @@ ORGANIZATION = None
 PROJECT = None
 
 def get_base_url():
+    """
+    Gets the base url used to access Transcriptic's services.
+
+    Returns
+    -------
+    str
+    """
     return BASE_URL
 
 def get_email():
+    """
+    Gets the email of the account used for Transcriptic's services.
+
+    Returns
+    -------
+    str
+    """
     if TRANSCRIPTIC_EMAIL is not None:
         return TRANSCRIPTIC_EMAIL
     return os.environ['TRANSCRIPTIC_EMAIL']
 
 def get_key():
+    """
+    Gets the private key used to access Transcriptic's services.
+
+    Returns
+    -------
+    str
+    """
     if TRANSCRIPTIC_KEY is not None:
         return TRANSCRIPTIC_KEY
     return os.environ['TRANSCRIPTIC_KEY']
 
 def get_organization():
+    """
+    Gets the currently active organization.
+
+    Returns
+    -------
+    str
+    """
     return ORGANIZATION
 
 def get_project():
-    return PROJECT
+    """
+    Gets the currently active project.
 
-def get_headers():
-    return {
-        "X-User-Email": get_email(),
-        "X-User-Token": get_key(),
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        }
+    Returns
+    -------
+    str
+    """
+    return PROJECT
--- pyscriptic/submit.py
+++ pyscriptic/submit.py
@@ -3,14 +3,38 @@ import requests, json
 
 from pyscriptic import settings
 
+def _get_headers():
+    """
+    Gets all headers needed to access Transcriptic's services.
+    """
+    return {
+        "X-User-Email": settings.get_email(),
+        "X-User-Token": settings.get_key(),
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        }
+
 def get_request(relative_url):
+    """
+    Sends a GET request (along will all necessary headers) to Transcript's
+    servers to a url relative to the base url of the service. Returns the json
+    response from the server.
+
+    Parameters
+    ----------
+    relative_url : str
+
+    Returns
+    -------
+    str
+    """
     url = "{}/{}".format(
         settings.get_base_url(),
         relative_url,
         )
     response = requests.get(
         url,
-        headers=settings.get_headers(),
+        headers=_get_headers(),
         )
     if response.status_code == 200:
         return response.json()
@@ -20,6 +44,21 @@ def get_request(relative_url):
             )
 
 def post_request(relative_url, content):
+    """
+    Sends a POST request (along will all necessary headers) to Transcript's
+    servers to a url relative to the base url of the service. Also sends
+    content, a payload that should be in json format, and returns the json
+    response.
+
+    Parameters
+    ----------
+    relative_url : str
+    content : str
+
+    Returns
+    -------
+    str
+    """
     url = "{}/{}".format(
         settings.get_base_url(),
         relative_url,
@@ -27,7 +66,7 @@ def post_request(relative_url, content):
     response = requests.post(
         url,
         content,
-        headers=settings.get_headers(),
+        headers=_get_headers(),
         )
     if response.status_code == 200:
         return response.json()
