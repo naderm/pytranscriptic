36db360442c682e3445e12f85624edbaf952cb66
Nader Morshed <morshed.nader@gmail.com>
2014-12-22 01:59:19 -0500

    Filled in more of the pipetting tests

--- pyscriptic/tests/instructions_test.py
+++ pyscriptic/tests/instructions_test.py
@@ -1,7 +1,8 @@
 
 from unittest import TestCase
 
-from pyscriptic.instructions import PipetteOp, TransferGroup, PrePostMix
+from pyscriptic.instructions import PipetteOp, PrePostMix, TransferGroup, \
+     DistributeGroup, ConsolidateGroup, MixGroup
 from pyscriptic.submit import pyobj_to_std_types
 
 class PipetteOpTests(TestCase):
@@ -53,10 +54,119 @@ class PipetteOpTests(TestCase):
         )
 
     def test_distribute(self):
-        pass
+        op = PipetteOp(
+            groups=[
+                DistributeGroup(
+                    to_wells=[
+                        dict(well="plate/A2", volume="10:microliter",
+                             aspirate_speed="1:microliter/second"),
+                        dict(well="plate/A3", volume="15:microliter",
+                             aspirate_speed="3:microliter/second"),
+                    ],
+                    from_well="plate/A1",
+                    aspirate_speed="2:microliter/second",
+                    mix_after=self.mix,
+                ),
+            ],
+        )
+        self.assertEqual(
+            pyobj_to_std_types(op),
+            {
+                "op": "pipette",
+                "groups": [{
+                    "distribute": [{
+                        "from": "plate/A1",
+                        "to": [
+                            {
+                                "well": "plate/A2",
+                                "volume": "10:microliter",
+                                "aspirate_speed": "1:microliter/second",
+                            },
+                            {
+                                "well": "plate/A3",
+                                "volume": "15:microliter",
+                                "aspirate_speed": "3:microliter/second",
+                            },
+                        ],
+                        "aspirate_speed": "2:microliter/second",
+                        "mix_after": {
+                            "volume": "5:microliter",
+                            "speed": "0.5:microliter/second",
+                            "repetitions": 10,
+                        },
+                    }]
+                }],
+            },
+        )
 
     def test_consolidate(self):
-        pass
+        op = PipetteOp(
+            groups=[
+                ConsolidateGroup(
+                    from_wells=[
+                        dict(well="plate/A2", volume="10:microliter",
+                             aspirate_speed="1:microliter/second"),
+                        dict(well="plate/A3", volume="15:microliter",
+                             aspirate_speed="3:microliter/second"),
+                    ],
+                    to="plate/A1",
+                    dispense_speed="2:microliter/second",
+                    mix_before=self.mix,
+                ),
+            ],
+        )
+        self.assertEqual(
+            pyobj_to_std_types(op),
+            {
+                "op": "pipette",
+                "groups": [{
+                    "consolidate": [{
+                        "to": "plate/A1",
+                        "from": [
+                            {
+                                "well": "plate/A2",
+                                "volume": "10:microliter",
+                                "aspirate_speed": "1:microliter/second",
+                            },
+                            {
+                                "well": "plate/A3",
+                                "volume": "15:microliter",
+                                "aspirate_speed": "3:microliter/second",
+                            },
+                        ],
+                        "dispense_speed": "2:microliter/second",
+                        "mix_before": {
+                            "volume": "5:microliter",
+                            "speed": "0.5:microliter/second",
+                            "repetitions": 10,
+                        },
+                    }]
+                }],
+            },
+        )
 
     def test_mix(self):
-        pass
+        op = PipetteOp(
+            groups=[
+                MixGroup(
+                    well="plate/A1",
+                    volume="20:microliter",
+                    speed="1:microliter/second",
+                    repetitions=10,
+                ),
+            ],
+        )
+        self.assertEqual(
+            pyobj_to_std_types(op),
+            {
+                "op": "pipette",
+                "groups": [{
+                    "mix": [{
+                        "well": "plate/A1",
+                        "volume": "20:microliter",
+                        "speed": "1:microliter/second",
+                        "repetitions": 10,
+                    }]
+                }],
+            },
+        )
