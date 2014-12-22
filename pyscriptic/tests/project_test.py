ff7c13efda6e222c2801545c3c84d7fd3f7b1e13
Nader Morshed <morshed.nader@gmail.com>
2014-12-22 15:04:14 -0500

    Test new project properties

--- pyscriptic/tests/project_test.py
+++ pyscriptic/tests/project_test.py
@@ -5,6 +5,15 @@ class ProjectTests(TestCase):
     def test_import_module(self):
         from pyscriptic.project import ProjectProperties, create_project, get_project
 
+    def test_new_project_properties(self):
+        from pyscriptic.project import ProjectProperties
+        ProjectProperties(
+            project_id="1234",
+            title="Project Title",
+            organization="Organization",
+            runs=[{"id": "run1", "status": "active"}],
+            )
+
     def test_create_project(self):
         pass
 
