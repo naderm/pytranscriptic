392f58abf7b163bb34e395f5818daa0a13d05342
Nader Morshed <morshed.nader@gmail.com>
2014-12-22 01:37:26 -0500

    Test conversion of Transfer to standard types works

--- pyscriptic/tests/instructions_test.py
+++ pyscriptic/tests/instructions_test.py
@@ -2,12 +2,13 @@
 from unittest import TestCase
 
 from pyscriptic.instructions import PipetteOp, TransferGroup, PrePostMix
+from pyscriptic.submit import pyobj_to_std_types
 
 class PipetteOpTests(TestCase):
     def setUp(self):
         self.mix = PrePostMix(
             volume="5:microliter",
-            speed="1:microliter/second",
+            speed="0.5:microliter/second",
             repetitions=10,
         )
 
@@ -19,11 +20,36 @@ class PipetteOpTests(TestCase):
                     to_well="plate/A2",
                     volume="20:microliter",
                     aspirate_speed="1:microliter/second",
-                    dispense_speed="1:microliter/second",
+                    dispense_speed="2:microliter/second",
                     mix_before=self.mix,
                     mix_after=self.mix,
                 ),
             ],
+        )
+        self.assertEqual(
+            pyobj_to_std_types(op),
+            {
+                "op": "pipette",
+                "groups": [{
+                    "transfer": [{
+                        "from": "plate/A1",
+                        "to": "plate/A2",
+                        "volume": "20:microliter",
+                        "aspirate_speed": "1:microliter/second",
+                        "dispense_speed": "2:microliter/second",
+                        "mix_after": {
+                            "volume": "5:microliter",
+                            "speed": "0.5:microliter/second",
+                            "repetitions": 10,
+                        },
+                        "mix_before": {
+                            "volume": "5:microliter",
+                            "speed": "0.5:microliter/second",
+                            "repetitions": 10,
+                        },
+                    }]
+                }],
+            },
         )
 
     def test_distribute(self):
