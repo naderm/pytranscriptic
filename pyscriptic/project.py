965b613c634599303a8a26cce18de7ea00d33fc8
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 23:37:07 -0500

    Added get_project and project_id to ProjectProperties

--- pyscriptic/project.py
+++ pyscriptic/project.py
@@ -6,13 +6,15 @@ class ProjectProperties(object):
 
     Attributes
     ----------
+    project_id : str
     title : str
     organization : str
     runs : list of dict of str, str
     """
-    def __init__(self, title, organization, runs=None):
+    def __init__(self, project_id, title, organization, runs=None):
         if runs is None:
             runs = []
+        self.project_id = project_id
         self.title = title
         self.organization = organization
         self.runs = runs
@@ -40,6 +42,22 @@ def create_project(project_id):
         content,
         )
     return ProjectProperties(
+        project_id=response["project_id"],
         title=response["title"],
         organization=response["organization"]["id"],
+        )
+
+def get_project(project_id):
+    url = "{}/{}".format(
+        settings.get_organization(),
+        project_id,
+        )
+    response = submit.get_request(
+        url,
+        )
+    return ProjectProperties(
+        project_id=response["project_id"],
+        title=response["title"],
+        organization=response["organization"]["id"],
+        runs=response["runs"],
         )
