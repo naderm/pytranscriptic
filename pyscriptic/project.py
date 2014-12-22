6562c72067019329b203ac0594b25d5bc7ba930b
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 23:05:09 -0500

    Added a docstring and removed the return statement for create_project

--- pyscriptic/project.py
+++ pyscriptic/project.py
@@ -2,13 +2,20 @@
 from pyscriptic import settings, submit
 
 def create_project(project_id):
+    """
+    Creates a new project within the currently active organization.
+
+    Parameters
+    ----------
+    project_id : str
+    """
     url = "{}".format(
         settings.get_organization(),
         )
     content = {
         "name": project_id,
         }
-    return submit.post_request(
+    submit.post_request(
         url,
         content,
         )
