f967c586dcbb923ef11778aafca8f1746f93dbb8
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 19:14:29 -0500

    Stubbed out some starting tests

---
+++ tests/instructions_test.py
@@ -0,0 +1,15 @@
+
+from unittest import TestCase
+
+class PipetteOpTests(TestCase):
+    def test_transfer(self):
+        pass
+
+    def test_distribute(self):
+        pass
+
+    def test_consolidate(self):
+        pass
+
+    def test_mix(self):
+        pass
