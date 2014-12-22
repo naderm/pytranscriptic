22a35bd1351eeb0f4061ae2905275d44073dfeff
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 19:33:10 -0500

    Added some content to test_transfer

--- pyscriptic/tests/instructions_test.py
+++ pyscriptic/tests/instructions_test.py
@@ -1,9 +1,30 @@
 
 from unittest import TestCase
 
+from pyscriptic.instructions import PipetteOp, TransferGroup, PrePostMix
+
 class PipetteOpTests(TestCase):
+    def setUp(self):
+        self.mix = PrePostMix(
+            volume="5:microliter",
+            speed="1:microliter/second",
+            repetitions=10,
+        )
+
     def test_transfer(self):
-        pass
+        op = PipetteOp(
+            groups=[
+                TransferGroup(
+                    from_well="plate/A1",
+                    to_well="plate/A2",
+                    volume="20:microliter",
+                    aspirate_speed="1:microliter/second",
+                    dispense_speed="1:microliter/second",
+                    mix_before=self.mix,
+                    mix_after=self.mix,
+                ),
+            ],
+        )
 
     def test_distribute(self):
         pass
