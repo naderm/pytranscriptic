a3d1c1097161e3845fb4bc657551b9f00ac3fa29
Nader Morshed <morshed.nader@gmail.com>
2014-12-21 23:31:52 -0500

    Added ProjectProperties class, which create project now returns an instance of

--- pyscriptic/project.py
+++ pyscriptic/project.py
@@ -1,6 +1,22 @@
 
 from pyscriptic import settings, submit
 
+class ProjectProperties(object):
+    """
+
+    Attributes
+    ----------
+    title : str
+    organization : str
+    runs : list of dict of str, str
+    """
+    def __init__(self, title, organization, runs=None):
+        if runs is None:
+            runs = []
+        self.title = title
+        self.organization = organization
+        self.runs = runs
+
 def create_project(project_id):
     """
     Creates a new project within the currently active organization.
@@ -8,6 +24,10 @@ def create_project(project_id):
     Parameters
     ----------
     project_id : str
+
+    Returns
+    -------
+    pyscriptic.project.ProjectProperties
     """
     url = "{}".format(
         settings.get_organization(),
@@ -15,7 +35,11 @@ def create_project(project_id):
     content = {
         "name": project_id,
         }
-    submit.post_request(
+    response = submit.post_request(
         url,
         content,
+        )
+    return ProjectProperties(
+        title=response["title"],
+        organization=response["organization"]["id"],
         )
