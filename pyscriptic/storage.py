abde982e8bd66f537b4d8f500c0cf8205f4b5861
Nader Morshed <morshed.nader@gmail.com>
2014-12-31 21:42:46 -0500

    PEP8 compliance

--- pyscriptic/storage.py
+++ pyscriptic/storage.py
@@ -6,6 +6,7 @@ Notes
 .. [1] https://www.transcriptic.com/platform/#instr_storage
 """
 
+
 class StorageType(object):
     """
     Attributes
@@ -15,20 +16,21 @@ class StorageType(object):
     def __init__(self, description):
         self.description = description
 
+
 STORAGE_LOCATIONS = {
     "ambient": StorageType(
         "may vary between 19.5 degC and 22 degC",
-        ),
+    ),
     "warm_37": StorageType(
         "may vary between 36 degC and 38 degC, optionally shaking",
-        ),
+    ),
     "cold_4": StorageType(
         "may vary between 3 degC and 5 degC",
-        ),
+    ),
     "cold_20": StorageType(
         "may vary between -22 degC and -18 degC",
-        ),
+    ),
     "cold_80": StorageType(
         "may vary between -84 degC and -76 degC",
-        ),
-    }
+    ),
+}
